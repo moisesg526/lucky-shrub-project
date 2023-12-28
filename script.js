639c6ca6364439e0f6e00f45cb475d7a157b3db3
moises gomez <moisesg526@yahoo.com>
2023-12-28 18:17:45 -0500

    Added function to display plant data from api

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+const display = document.querySelector("#display-data");
+
 let APIKey = "sk-SVAZ658cd7515e87f3600";
 var requestOptions = {
   method: "GET",
   redirect: "follow",
 };
 
-fetch(
-  "https://perenual.com/api/species-list?key=sk-SVAZ658cd7515e87f3600",
-  requestOptions
-)
-  .then((response) => response.json())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+const getData = async () => {
+  const res = await fetch(
+    `https://perenual.com/api/species-list?key=${APIKey}`,
+    requestOptions
+  );
+  const data = await res.json();
+  return data;
+};
+
+const displayPlants = async () => {
+  const callData = await getData();
+
+  let displayData = callData.data.map((object) => {
+    const { common_name, watering } = object;
+
+    return `
+    <div class="continer">
+        <p>Name: ${common_name}</p> 
+        <p>Watering: ${watering}</p>
+    </div>
+    `;
+  });
+
+  display.innerHTML = displayData;
+};
+
+displayPlants();
